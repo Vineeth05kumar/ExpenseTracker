6d202c34bfa5cd5271a4a1c6d28bf338a100facc
Vineeth05kumar <Vineeth05kumar>
2024-08-06 10:55:34 +0530

    Verified the email using firebase

--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -1,22 +1,49 @@
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-
 export default function Welcome() {
-    
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
-    const navigate = useNavigate();
-    const changePage = () =>{
-        navigate('/details')
+  const emailVerifyHandler = async () => {
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBcTzMy6yeRL1-JVvi7Rse7eIBsR1Q1mes",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: token,
+          }),
+        }
+      );
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error.message);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
     }
+  };
+
+  const changePage = () => {
+    navigate("/details");
+  };
+
   return (
     <Container>
       <h1>Welcome inside expense tracker</h1>
 
       <h4>
         Your Profile is Incomplete
-        <Button varient="link" onClick={changePage}>Click to complete</Button>
+        <Button variant="link" onClick={changePage}>Click to complete</Button>
       </h4>
+      <Button variant="danger" onClick={emailVerifyHandler}>Verify Email</Button>
     </Container>
   );
 }
